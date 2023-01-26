293d0813d58bba60a5614560e4f74517ebe2469d
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-26 15:21:15 -0600

    Notes html route In this commit created the GET route for notes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,4 +7,8 @@ const app = express();
 //Middleware sets the static pages inside the public folder
 app.use(express.static('public'));
 
+//Notes route
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 app.listen(PORT, () => console.log(`Listenining on port: ${PORT}`));
