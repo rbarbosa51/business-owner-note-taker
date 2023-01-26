6b4601113e8aba18aa38311a01138ea0c7b58a77
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-26 15:23:58 -0600

    Wildcard Route In this commit created a wildcard route directing all traffic to the index.html file

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ app.use(express.static('public'));
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+//* Route
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 app.listen(PORT, () => console.log(`Listenining on port: ${PORT}`));
