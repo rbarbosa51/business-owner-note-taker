77fd110d4d0477ad2cbb34fee0a475bfdf6e6149
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-26 15:34:00 -0600

    Notes API routes Experimenting with routes

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const notes = require('./notes.js');
+
+const app = express();
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
 //Middleware sets the static pages inside the public folder
 app.use(express.static('public'));
 
