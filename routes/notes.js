c36f51d6303959ad80c35e0b75d0c7f068746bfb
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-26 16:14:51 -0600

    Notes get route Now the route returns JSON data that gets parsed to the UI on the front end

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+/*Using fs/promises to use readFile as a promise, 
+without having to use utils to promisity */
+const fs = require('fs/promises');
 
-router.get('/', (req,res) => {
-    console.log(req.params);
+
+router.get('/', async (req,res) => {
+    const data = await fs.readFile('./db/db.json', {encoding: 'utf-8'});
+    console.log(data);
+    res.json(JSON.parse(data));
 })
 router.post('/', (req,res) => {
     //Debugging
