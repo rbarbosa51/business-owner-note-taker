8e28a1c01c8eaa5dcf7ba1f87fab30a9281a9c04
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-26 23:35:36 -0600

    POST Request write Writting the new data recived to post to the JSON file.  There is still no id.

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,29 @@
 const router = require('express').Router();
-/*Using fs/promises to use readFile as a promise, 
+/*Using fs/promises to use readFile and writeFile as promises, 
 without having to use utils to promisity */
 const fs = require('fs/promises');
+const dbFile = './db/db.json';
 
-
-router.get('/', async (req,res) => {
-    const data = await fs.readFile('./db/db.json', {encoding: 'utf-8'});
-    console.log(data);
+router.get('/', async (_,res) => {
+    const data = await fs.readFile(dbFile, {encoding: 'utf-8'});
+    //console.log(data);
     res.json(JSON.parse(data));
-})
-router.post('/', (req,res) => {
-    //Debugging
-    console.log(req.body);
-})
+});
+router.post('/', async (req,res) => {
+    //Get File and store values in array
+    const rawData = await fs.readFile(dbFile, {encoding: 'utf-8'});
+    let newData = JSON.parse(rawData);
+    //Push the body values into array
+    newData.push(req.body);
+    //Debug
+    console.log(newData);
+    //Write to JSON file
+    await fs.writeFile(dbFile, JSON.stringify(newData));
+    res.json(newData);
+    
+    
+});
+router.delete('/', (req, res) => {
+    console.log(req);
+});
 module.exports = router;
