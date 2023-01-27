1b7755aaa78e497e39dff8c99f26a7dbff270df2
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-27 00:49:01 -0600

    Prettier Used prettier to "prettify" the code.

--- routes/notes.js
+++ routes/notes.js
@@ -1,48 +1,46 @@
-const router = require('express').Router();
+const router = require("express").Router();
 //UUID
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 /*Using fs/promises to use readFile and writeFile as promises, 
 without having to use utils to promisity */
-const fs = require('fs/promises');
-const dbFile = './db/db.json';
+const fs = require("fs/promises");
+const dbFile = "./db/db.json";
 
-router.get('/', async (_,res) => {
-    const data = await fs.readFile(dbFile, {encoding: 'utf-8'});
-    res.json(JSON.parse(data));
+router.get("/", async (_, res) => {
+  const data = await fs.readFile(dbFile, { encoding: "utf-8" });
+  res.json(JSON.parse(data));
 });
-router.post('/', async (req,res) => {
-    //Parse request body into an object
-    const {title, text} = req.body;
-    const id = uuidv4();
-    const pData = {id, title,text};
-    //Get File and store values in array of objects
-    const rawData = await fs.readFile(dbFile, {encoding: 'utf-8'});
-    let newData = JSON.parse(rawData);
-    //Push the body values into array
-    newData.push(pData);
-    //Debug Only
-    //console.log(newData);
-    //Write to JSON file
-    await fs.writeFile(dbFile, JSON.stringify(newData));
-    res.json(newData);
-    
+router.post("/", async (req, res) => {
+  //Parse request body into an object
+  const { title, text } = req.body;
+  const id = uuidv4();
+  const pData = { id, title, text };
+  //Get File and store values in array of objects
+  const rawData = await fs.readFile(dbFile, { encoding: "utf-8" });
+  let newData = JSON.parse(rawData);
+  //Push the body values into array
+  newData.push(pData);
+  //Debug Only
+  //console.log(newData);
+  //Write to JSON file
+  await fs.writeFile(dbFile, JSON.stringify(newData));
+  res.json(newData);
 });
-router.delete('/:id', async (req, res) => {
-    console.log(req.params.id);
-    //req.params.id contains the uuid of the item to be deleted
-    //Get all of the db and convert to array
-    const rawData = await fs.readFile(dbFile, {encoding: 'utf-8'});
-    let primeData = JSON.parse(rawData);
-    //find the selected id
-    const selectedId = primeData.find(d => d.id === req.params.id);
-    //Get the index of the selected id and then use slice to delete it
-    const index = primeData.indexOf(selectedId);
-    primeData.splice(index,1);
-    //debug
-    //console.log(JSON.stringify(primeData));
-    //write new db.json
-    await fs.writeFile(dbFile, JSON.stringify(primeData));
-    res.json(primeData);
-
+router.delete("/:id", async (req, res) => {
+  console.log(req.params.id);
+  //req.params.id contains the uuid of the item to be deleted
+  //Get all of the db and convert to array
+  const rawData = await fs.readFile(dbFile, { encoding: "utf-8" });
+  let primeData = JSON.parse(rawData);
+  //find the selected id
+  const selectedId = primeData.find((d) => d.id === req.params.id);
+  //Get the index of the selected id and then use slice to delete it
+  const index = primeData.indexOf(selectedId);
+  primeData.splice(index, 1);
+  //debug
+  //console.log(JSON.stringify(primeData));
+  //write new db.json
+  await fs.writeFile(dbFile, JSON.stringify(primeData));
+  res.json(primeData);
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const api = require('./routes/index.js');
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,18 +8,18 @@ const app = express();
 //Middleware needed for API calls
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use("/api", api);
 
 //Middleware sets the static pages inside the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Notes route
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 //* (All Other) Routes
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 //Listen
